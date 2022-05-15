612a2a30e07443d98bec77931c9f3db3dc022d0b
Mārcis Kronlaks <s8_kronla_m@venta.lv>
2022-05-15 21:31:59 +0300

    Final commit

--- cypress/integration/WEB_AUT_Marcis_Kronlaks/elements.spec.js
+++ cypress/integration/WEB_AUT_Marcis_Kronlaks/elements.spec.js
@@ -2,21 +2,75 @@ import PraticeForm from '../../pageObjects/praticeForm';
 
 
 context("Pratice Form", () => {
-    context("Pratice Form scenarios", () => {
-        beforeEach(() => {
+
+
+    it("Filling in Text Boxes", () => {
+        cy.fixture("praticeFormData").then((data) => {
             PraticeForm.visit();
-        });
+        
 
 
-        it.only("Filling in Text Boxes", () => {
+        
             //add scenario stuf here
-            PraticeForm.firstNameInput.type("James");
-            PraticeForm.lastNameInput.type("Oak");
-            PraticeForm.userEmailInput.type("jamesoak@gmai.com");
+            PraticeForm.firstNameInput.type(data.firstname);
+            PraticeForm.lastNameInput.type(data.lastname);
+            PraticeForm.userEmailInput.type(data.email);
+
             PraticeForm.maleRadioButton.click();
-            PraticeForm.userNumberInput.type("3457758979")
-        })
 
+            PraticeForm.userNumberInput.type(data.mobile);
+
+            PraticeForm.dateOfBirthInput.click();
+            PraticeForm.yearInput.select("1930");
+            PraticeForm.monthInput.select("February");
+            PraticeForm.dayInput.click();
+
+            PraticeForm.subjectsInput.type(data.subjects);
+            PraticeForm.addSubjects.click();
+
+            PraticeForm.hobbiesMusic.click();
+
+            PraticeForm.uploadFile.selectFile('cypress/files/NGNL.png');
+
+            PraticeForm.stateInput.click();
+            PraticeForm.selectNCR.click();
+
+            PraticeForm.city.click();
+            PraticeForm.selectDelhi.click();
+
+            PraticeForm.submitButton.click();
+
+
+
+        });
     });
-});
 
+    it("Validating data", () => {
+        cy.fixture("praticeFormData").then((data) => {
+
+        PraticeForm.TableRowCell("Student Name")
+        .should("contain", data.fullname);
+        PraticeForm.TableRowCell("Student Email")
+        .should("contain", data.email);
+        PraticeForm.TableRowCell("Gender")
+        .should("contain", data.gender);
+        PraticeForm.TableRowCell("Mobile")
+        .should("contain", data.mobile);
+        PraticeForm.TableRowCell("Date of Birth")
+        .should("contain", data.dateofbirth);
+        PraticeForm.TableRowCell("Subjects")
+        .should("contain", data.subjects);
+        PraticeForm.TableRowCell("Hobbies")
+        .should("contain", data.hobbies);
+        PraticeForm.TableRowCell("Picture")
+        .should("contain", data.picture);
+        PraticeForm.TableRowCell("Address")
+        .should("contain", data.address);
+        PraticeForm.TableRowCell("State and City")
+        .should("contain", data.stateAndCity);
+
+
+        });
+    });
+
+});
--- cypress/pageObjects/praticeForm.js
+++ cypress/pageObjects/praticeForm.js
@@ -21,6 +21,10 @@ class PraticeForm extends BasePage {
     return cy.get('input#userEmail');
   }
 
+  static get userCurrentAdressInput () {
+    return cy.get('input#currentAddress');
+  }
+
   static get maleRadioButton() {
     return cy.get("label[for= 'gender-radio-1']");
   }
@@ -37,6 +41,70 @@ class PraticeForm extends BasePage {
     return cy.get("input#userNumber");
   }
 
+  static get dateOfBirthInput () {
+    return cy.get('input#dateOfBirthInput');
+  }
+
+  static get yearInput () {
+    return cy.get('*[class^="react-datepicker__year-select"]');
+  }
+
+  static get monthInput () {
+    return cy.get('*[class^="react-datepicker__month-select"]');
+  }
+
+  static get dayInput () {
+    return cy.get('*[class^="react-datepicker__day react-datepicker__day--025"]');
+    // Works for any number until 26 after that does not work for some reason
+  }
+
+  static get subjectsInput () {
+    return cy.get('input#subjectsInput');
+  }
+
+  static get addSubjects () {
+    return cy.get('#react-select-2-option-0');
+  }
+
+  static get hobbiesMusic () {
+    return cy.get("label[for='hobbies-checkbox-3']");
+  }
+
+  static get uploadFile () {
+    return cy.get('input[type=file]');
+  }
+
+  static get stateInput () {
+    return cy.get('*[class^=" css-yk16xz-control"]');
+  }
+
+  static get selectNCR () {
+    return cy.get('#react-select-3-option-0');
+  }
+
+  static get city () {
+    return cy.get('*[class^=" css-1wa3eu0-placeholder"]');
+  }
+
+  static get selectDelhi () {
+    return cy.get('#react-select-4-option-0');
+  }
+
+  static get submitButton () {
+    return cy.get('*[class^="btn btn-primary"]');
+  }
+
+  static get formData () {
+    return cy.get('td:nth-child(2)');
+  }
+
+  static TableRowCell(label) {
+    return cy.get("tr>td")
+      .contains(label)
+      .parent()
+      .find('td');
+  };
+  
 }
 
 
